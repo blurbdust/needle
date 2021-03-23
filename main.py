d0033fd240cc6512f6bf82678a68701233554f25
blurbdust <blurbdust@gmail.com>
2021-03-23 15:51:51 -0500

    now checks for all occurances within the chunk

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import os, sys, uuid
-#import hexdump, re
+import hexdump, re
 
 SAM_pattern		= b"\\\x00S\x00y\x00s\x00t\x00e\x00m\x00R\x00o\x00o\x00t\x00\\\x00S\x00y\x00s\x00t\x00e\x00m\x003\x002\x00\\\x00C\x00o\x00n\x00f\x00i\x00g\x00\\\x00S\x00A\x00M"
 SYSTEM_pattern		= b"\x00S\x00Y\x00S\x00T\x00E\x00M\x00\x00\x00\x00\x00"
@@ -26,6 +26,16 @@ filename = sys.argv[1]
 f = open(filename, 'rb')
 f_size = os.stat(filename).st_size
 
+# https://stackoverflow.com/questions/4664850/how-to-find-all-occurrences-of-a-substring
+def cust_findall(string, substring):
+	substring_length = len(substring)
+	def recurse(locations_found, start):
+		location = string.find(substring, start)
+		if location != -1:
+			return recurse(locations_found + [location], location+substring_length)
+		else:
+			return locations_found
+	return recurse([], 0)
 
 def autodump(sam, system, security, ntds):
 	# no need to keep scanning as we found all three
@@ -59,20 +69,19 @@ def autodump(sam, system, security, ntds):
 				__LSASecrets = LSASecrets(_security, bootKey, None, isRemote=False, history=False)
 				__LSASecrets.dumpSecrets()
 	except NameError:
-		print("impacket not installed")
 		print("Not able to auto parse hives using existing tools. Please install impacket or manually check and dump the registry hives.")
 	except:
 		pass
-	sys.exit(0)
+	#sys.exit(0)
 
 
-def search_chunk(chunk, chunk_num, chunk_size):
-	temp_SAM = chunk.find(SAM_pattern)
-	temp_SYSTEM = chunk.find(SYSTEM_pattern)
-	temp_SECURITY = chunk.find(SECURITY_pattern)
-	temp_NTDS = chunk.find(NTDS_pattern)
+def search_chunk(chunk, chunk_num, chunk_size, misaligned):
+	_temp_SAM = cust_findall(chunk, SAM_pattern)
+	_temp_SYSTEM = cust_findall(chunk, SYSTEM_pattern)
+	_temp_SECURITY = cust_findall(chunk, SECURITY_pattern)
+	_temp_NTDS = cust_findall(chunk, NTDS_pattern)
 
-	if (temp_SAM > -1):
+	for temp_SAM in _temp_SAM:
 		# potential SAM found
 		# print(hexdump.hexdump(chunk[temp_SAM - 0x30 : temp_SAM + len(SAM_pattern)]))
 		if (b"regf" in chunk[temp_SAM - 0x30 : temp_SAM - 0x30 + 0x4]):
@@ -81,26 +90,32 @@ def search_chunk(chunk, chunk_num, chunk_size):
 			print("Potentially found SAM at offset {} within searched chunk {}. Writing to {}".format(temp_SAM, chunk_num, tmp_name))
 			with open(tmp_name, "wb") as SAM:
 				g = open(filename, 'rb')
-				g.seek(((chunk_num - 1) * chunk_size) + (temp_SAM - 0x30))
+				if misaligned:
+					g.seek((chunk_size) + ((chunk_num - 1) * chunk_size) + (temp_SAM - 0x30))
+				else:
+					g.seek(((chunk_num - 1) * chunk_size) + (temp_SAM - 0x30))
 				# 16MB is supposedly max size of registry hives on disk; impacket doesn't seem to have a problem with extra data at the end of the registry hives.
 				SAM.write(g.read(min(f_size, 2800000)))
 				g.close()
 			found[0] = True
-	if (temp_SYSTEM > -1):
+	for temp_SYSTEM in _temp_SYSTEM:
 		# potential SYSTEM found; 0x2F since we search by \x00S to rule out false positives
-		# print(hexdump.hexdump(chunk[temp_SYSTEM - 0x2F : temp_SYSTEM + len(SYSTEM_pattern)]))
 		if (b"regf" in chunk[temp_SYSTEM - 0x2F : temp_SYSTEM - 0x2F + 0x4 ]):
 			tmp_name = str(uuid.uuid4()) + "_SYSTEM"
 			SYSTEM_filenames.append(tmp_name)
 			print("Potentially found SYSTEM at offset {} within searched chunk {}. Writing to {}".format(temp_SYSTEM, chunk_num, tmp_name))
+			#print(hexdump.hexdump(chunk[temp_SYSTEM - 0x2F : temp_SYSTEM + len(SYSTEM_pattern)]))
 			with open(tmp_name, "wb") as SYSTEM:
 				g = open(filename, 'rb')
-				g.seek(((chunk_num - 1) * chunk_size) + (temp_SYSTEM - 0x2F))
+				if misaligned:
+					g.seek((chunk_size) + ((chunk_num - 1) * chunk_size) + (temp_SYSTEM - 0x2F))
+				else:
+					g.seek(((chunk_num - 1) * chunk_size) + (temp_SYSTEM - 0x2F))
 				# 16MB is supposedly max size of registry hives on disk; impacket doesn't seem to have a problem with extra data at the end of the registry hives.
-				SYSTEM.write(g.read(min(f_size, 16000000)))
+				SYSTEM.write(g.read(min(f_size, 17000000)))
 				g.close()
 			found[1] = True
-	if (temp_SECURITY > -1):
+	for temp_SECURITY in _temp_SECURITY:
 		# potential SECURITY found
 		# print(hexdump.hexdump(chunk[temp_SECURITY - 0x30 : temp_SECURITY + len(SECURITY_pattern)]))
 		if (b"regf" in chunk[temp_SECURITY - 0x30 : temp_SECURITY - 0x30 + 0x4]):
@@ -109,12 +124,15 @@ def search_chunk(chunk, chunk_num, chunk_size):
 			print("Potentially found SECURITY at offset {} within searched chunk {}. Writing to {}".format(temp_SECURITY, chunk_num, tmp_name))
 			with open(tmp_name, "wb") as SECURITY:
 				g = open(filename, 'rb')
-				g.seek(((chunk_num - 1) * chunk_size) + (temp_SECURITY - 0x30))
+				if misaligned:
+					g.seek((chunk_size) + ((chunk_num - 1) * chunk_size) + (temp_SECURITY - 0x30))
+				else:
+					g.seek(((chunk_num - 1) * chunk_size) + (temp_SECURITY - 0x30))
 				# 16MB is supposedly max size of registry hives on disk; impacket doesn't seem to have a problem with extra data at the end of the registry hives.
 				SECURITY.write(g.read(min(f_size, 16000000)))
 				g.close()
 			found[2] = True
-#	if (temp_NTDS > -1):
+#	for temp_NTDS in _temp_NTDS:
 #		# potential NTDS found
 #		if (b"\x00\x00\x00\x00\x00" in chunk[temp_NTDS + 8 + 3 : temp_NTDS + 11 + 5]):
 #			if (b"\x00\x00\x00" not in chunk[temp_NTDS + 8 : temp_NTDS + 8 + 3]):
@@ -133,6 +151,12 @@ def search_chunk(chunk, chunk_num, chunk_size):
 #								g.close()
 #							found[3] = True
 
+def check():
+	# shoutout to @knavesec for this monstrosity, summing across a list of bools hurts me
+	if ((found[1] == True) and (sum(found) >= 2)):
+		autodump(found[0], found[1], found[2], found[3])
+
+
 def main():
 	# reading in chunks and scanning through the chunks, if we don't find anything, maybe our chunks were too small and the pattern was at the boundry of chunks so we need to seek by chunk / 2 and scan again
 
@@ -145,7 +169,7 @@ def main():
 		f.seek(start)
 		chunk = f.read(chunk_size)
 		chunk_num += 1
-		if (search_chunk(chunk, chunk_num, chunk_size) == True):
+		if (search_chunk(chunk, chunk_num, chunk_size, False) == True):
 			break
 		start = end
 		end += chunk_size
@@ -153,23 +177,23 @@ def main():
 	# finish last partial chunk just in case
 	f.seek(start)
 	chunk = f.read(f_size - start)
-	search_chunk(chunk, chunk_num, chunk_size)
+	search_chunk(chunk, chunk_num, chunk_size, False)
 
-	# shoutout to @knavesec for this monstrosity, summing across a list of bools hurts me
-	if ((found[1] == True) and (sum(found) >= 2)):
-		autodump(found[0], found[1], found[2], found[3])
+	check()
 
 	# misalign and search the new chunks in case the \x00S\x00A\x00M and regf are across chunk boundries
-
-	start = chunk_size // 2
+	chunk_size = chunk_size // 4
+	start = chunk_size
 	end = start + chunk_size
+	chunk_num = 0
 	while (end < f_size):
 		f.seek(start)
 		chunk = f.read(chunk_size)
 		chunk_num += 1
-		if (search_chunk(chunk, chunk_num, chunk_size) == True):
+		if (search_chunk(chunk, chunk_num, chunk_size, True) == True):
 			break
 		start = end
 		end += chunk_size
 
+	check()
 main()
